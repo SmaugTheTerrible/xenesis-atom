bac6469120f472d8f975814bf3f6b4564321623e
SmaugTheTerrible <x.smaug.the.terrible@gmail.com>
2020-03-17 17:27:26 +0300

    Initial commit

---
+++ spec/xenesis-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import XenesisAtomView from '../lib/xenesis-atom-view';
+
+describe('XenesisAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
